b40e489c072f861765a1e5ddeb84df77cb515364
Suman More <sumanmore322@gmail.com>
2023-03-25 21:24:46 +0530

    Restructured the format of logs view

--- frontend/src/components/fulllogs.js
+++ frontend/src/components/fulllogs.js
@@ -1,36 +1,53 @@
 import { useState } from "react";
-import { Grid, List, ListItem, ListItemText, Divider } from '@mui/material';
-import { Pagination } from "@mui/lab";
+import {List,Grid, ListItem, ListItemText, Divider,ListItemAvatar,Avatar,Pagination} from '@mui/material';
 import { logsData } from '../services.js/data'
-import UsePagination from '../services.js/pagination'
+import UsePagination from '../services.js/pagination';
+import SendIcon from '@mui/icons-material/Send';
+import ReplyIcon from '@mui/icons-material/Reply';
+
+import * as React from 'react';
 
 const Logs = () => {
   const [page, setPage] = useState(1);
-  const PER_PAGE = 8;
+  const PER_PAGE = 6;
   const count = Math.ceil(logsData.users.length / PER_PAGE);
   const _DATA = UsePagination(logsData.users, PER_PAGE);
   const handleChange = (e, p) => { setPage(p); _DATA.jump(p); };
-
+ 
   return (
-    <Grid sx={{ bgcolor: 'white', width: 1 / 2, mx: 'auto', my: 2, borderRadius: 2, "@media (max-width: 768px)": { width: '100%' } }}>
+    <Grid sx={{ bgcolor: 'white', width:'330px', mx: 'auto', my: 2, borderRadius: 2, "@media (max-width: 768px)": { width: '100%' } }}>
       <List sx={{}}>
-        {_DATA.currentData().map(data => (
-          <>
-            <ListItem key={data.id}>
-              <ListItemText primary={data.address.city} secondary={data.birthDate} />
+        {_DATA.currentData().map((data, index) => (
+          <React.Fragment key={data.id}>
+            <ListItem key={data.id} alignItems="center">
+              <ListItemAvatar>
+                <Avatar>
+                  {data.gender === 'male' ? <SendIcon /> : <ReplyIcon />}
+                </Avatar>
+              </ListItemAvatar>
+              <ListItemText
+                primary={data.firstName}
+                secondary={
+                  <>
+                    <span>{data.birthDate}</span>
+                    <br />
+                    <span>{data.height}</span>
+                  </>
+                }
+              />
             </ListItem>
             <Divider variant="inset" component="li" />
-          </>
-
+          </React.Fragment>
         ))}
       </List>
       <Pagination
-        count={count}
-        page={page}
-        onChange={handleChange}
-        sx={{ float: 'right' }}
-      />
+          count={count}
+          page={page}
+          onChange={handleChange}
+          sx={{float: 'right'}}
+        />
     </Grid>
-  )
+  );
 }
-export default Logs
+
+export default Logs;
