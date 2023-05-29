12eb58e4f52ef59db5747fe68373d27c3ee49248
Rohan Verma <rohanvermaara@gmail.com>
2023-05-29 13:03:35 +0530

    purged

--- frontend/src/services.js/data.js
+++
@@ -1,5 +0,0 @@
-export let qrData;
-fetch('https://jsonplaceholder.typicode.com/users').then(response => response.json()).then(data => { qrData = JSON.stringify(data[0].name); });
-
-export let logsData;
-fetch('https://dummyjson.com/users').then(response => response.json()).then(data => { logsData = data; });
